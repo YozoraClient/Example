5ba205ae16a54a9b6c265f2b759fc4563e75f963
YozoraClient <YozoraClient@gmail.com>
2024-03-15 22:55:33 +0900

    Uploaded example-1

---
+++ Example_1/Example_1/dllmain.cpp
@@ -0,0 +1,31 @@
+﻿#include <windows.h>
+#include <iostream>
+
+void start()
+{
+    if (!GetConsoleWindow())
+    {
+        AllocConsole();
+        SetConsoleTitleA("Example 1 - Setting up Console");
+        FILE* out;
+        freopen_s(&out, "CONOUT$", "w", stdout);
+        freopen_s(&out, "CONIN$", "r", stdin);
+    }
+    else {
+        ShowWindow(GetConsoleWindow(), SW_SHOW);
+    }
+    
+    std::cout << "Hello World!" << std::endl; 
+}
+
+BOOL APIENTRY DllMain(HMODULE module,DWORD callReason, LPVOID reserved)
+{
+    switch (callReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(module);
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)start, module, 0, 0);
+    }
+    return TRUE;
+}
+
